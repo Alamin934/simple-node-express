3a2e90b35ce3088bbb83096dc22da8b8f77c3a48
Alamin934 <islamalamin934@gmail.com>
2022-06-01 15:30:09 +0600

    lunch node js with express js

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
-const users = [
-    { id: 0, name: 'alamin', email: 'alamin@gmail.com', phone: '018545464545' },
-    { id: 1, name: 'sojib', email: 'sojib@gmail.com', phone: '018545464545' },
-    { id: 2, name: 'hamid', email: 'hamid@gmail.com', phone: '018545464545' },
-    { id: 3, name: 'nahid', email: 'nahid@gmail.com', phone: '018545464545' },
-]
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+
+
+
+
+
+
+
+
+// const users = [
+//     { id: 0, name: 'alamin', email: 'alamin@gmail.com', phone: '018545464545' },
+//     { id: 1, name: 'sojib', email: 'sojib@gmail.com', phone: '018545464545' },
+//     { id: 2, name: 'hamid', email: 'hamid@gmail.com', phone: '018545464545' },
+//     { id: 3, name: 'nahid', email: 'nahid@gmail.com', phone: '018545464545' },
+// ]
+
